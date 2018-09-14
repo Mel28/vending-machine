1fe3766f01096bb595e0f6359221dc7aa6b01aac
Melanie Milsted <melanie.milsted@gmail.com>
2018-09-14 05:06:23 +0000

    additional coins argument, removal of if statements additional tests

--- vending-machine.py
+++ vending-machine.py
@@ -1,18 +1,16 @@
 from byotest import *
 
+coins = [100, 50, 20, 10, 5, 2, 1]
+
 def get_change(amount):
-    if amount == 0:
-     return []
-    if amount in [100, 50, 20, 10, 5, 2, 1]:
-     return [amount]
     
     change = []
-    for coin in [100, 50, 20, 10, 5, 2, 1]:
-        if coin <= amount:
+    for coin in coins:
+       while coin <= amount:
             amount -= coin
             change.append(coin)
             
-            return change
+    return change
     
 test_are_equal(get_change(0),[])
 test_are_equal(get_change(1),[1])
@@ -22,5 +20,8 @@ test_are_equal(get_change(10),[10])
 test_are_equal(get_change(20),[20])
 test_are_equal(get_change(50),[50])
 test_are_equal(get_change(100),[100])
+test_are_equal(get_change(3),[2,1])
+test_are_equal(get_change(7),[5,2])
+test_are_equal(get_change(9),[5,2,2])
 
 print("All Tests Pass!")
